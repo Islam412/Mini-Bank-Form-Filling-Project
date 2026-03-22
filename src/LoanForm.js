6d510851a267a1993215041342b90e11ed189084
Islam412 <islam.124.hamdy@gmail.com>
2026-03-23 01:19:25 +0200

    create form p1 and create style form p1

---
+++ src/LoanForm.js
@@ -0,0 +1,34 @@
+import "./FormStyle.css";
+
+export default function LoanForm() {
+    return (
+        <div className="flex container">
+            <form className="form" id="loan-form">
+                <h1>Requesting a Loan</h1>
+                <hr/>
+
+                <label>Name: </label>
+                <input />
+
+                <label>Phone Number: </label>
+                <input />
+
+                <label>Age: </label>
+                <input />
+
+                <label style={{ marginTop: '30px' }}>Are you an employee? </label>
+                <input type="checkbox" />
+
+                <label>Salary </label>
+                <select>
+                    <option>Less than 500$</option>
+                    <option>Less than 400$</option>
+                    <option>Less than 300$</option>
+                    <option>Less than 200$</option>
+                </select>
+
+                <button id="submit-loan-btn">Submit</button>
+            </form>
+        </div>
+    )
+}
